464513e17befa8ca2d4cd1d8d845f28d9db8da9f
lazerguy444 <65039257+lazerguy444@users.noreply.github.com>
2024-04-11 09:15:06 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,22 @@
 const config = {
   challenge: false, // Set to true if you want to enable password protection.
   users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
+    Parzival: '978*amit',
+    Thenryj28: 'random_Name',
+    A_DJ: '12457810',
+    Toucan:'Toucan#1',
+    AnimeFrogs: 'Frogsarecool69!',
+    JohnBarosa: 'MeanyFiddle',
+    Pancakewolfy: 'capybar3',
+    memes: 'memes@6751',
+    iluv: 'men',
+    DEEFERF4: '0911228843',
+    bob:  'bobby',
+    shanedawson:  'password',
+    ihatemrleonard:  'goofygoober',
+    tacomuncher:  'burritos',
+    albert:  '1226',
+    urmom:  '0622',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
